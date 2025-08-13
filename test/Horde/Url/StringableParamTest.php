d574b89628a855d5debe8d8b7ef1c806949e2c30
Torben Dannhauer <torben@dannhauer.de>
2025-08-13 22:09:12 +0200

    Create StringableParamTest.php

---
+++ test/Horde/Url/StringableParamTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author     Torben Dannhauer / GPT-5
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @category   Horde
+ * @package    Url
+ * @subpackage UnitTests
+ */
+
+namespace Horde\Url;
+use \PHPUnit\Framework\TestCase;
+use \Horde_Url;
+
+class StringableParamTest extends TestCase
+{
+	private function getStringableObject($value)
+	{
+		return new class($value) {
+			private $value;
+			public function __construct($value) { $this->value = $value; }
+			public function __toString() { return (string)$this->value; }
+		};
+	}
+
+	public function testStringableObjectAsParamValue()
+	{
+		$this->markTestIncomplete('Generalized normalization for all stringable objects may be implemented later.');
+		$url = new Horde_Url('test');
+		$url->add('s', $this->getStringableObject('a&b'));
+		// Current implementation only normalizes Horde_Url instances; generic stringables are not cast explicitly.
+		// Keep test incomplete until generalized handling is agreed upon.
+	}
+}
